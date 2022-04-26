880544fc47a498dea7294d593dfe6694d774d4f1
Nicole Hofstetter <nicole.hofstetter@audi.de>
2022-04-26 11:19:13 +0200

    feat: Setup server listening on port 8080

--- server.js
+++ server.js
@@ -2,18 +2,30 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
-// Setup Server.gitignore
+// Setup Server
+const port = 8080;
+
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log('Server is running');
+}
