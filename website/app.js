f6c92680a2b5b817b70b336828dc3ec06b645ece
Nicole Hofstetter <nicole.hofstetter@audi.de>
2022-05-17 21:08:29 +0200

    feat : Implement client side
    Gets data from open weather api,
    posts data to server,
    and updates ui with the values.

--- website/app.js
+++ website/app.js
@@ -1,5 +1,67 @@
 /* Global Variables */
+const apiKey = '0ef9c9d6a94d15f124e1e66e268c26ee&units=imperial';
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const getTemperatureByZip = async (zipCode) => {
+    let url = 'https://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + ',de&appid=' + apiKey ;
+    const response = await fetch(url);
+    try {
+        const weatherResponse = await response.json();
+        return weatherResponse && weatherResponse.main.temp;
+    } catch (error) {
+        console.log('Can not retrieve current weather data', error);
+    }
+};
+
+function buildRecentUserEntry(temperature) {
+    let userInput = document.getElementById('feelings').value;
+    let date = new Date();
+    let newDate = date.getMonth() + '.' + date.getDate() + '.' + date.getFullYear();
+
+    return {
+        'temperature': temperature,
+        'date': newDate,
+        'userResponse': userInput
+    };
+}
+
+const postRecentUserEntry = async (recentUserEntry) => {
+    const response = await fetch('api/user-entry', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(recentUserEntry)
+    });
+
+    try {
+        const responseData = await response.json();
+        console.log(responseData);
+    } catch (error) {
+        console.log('Could not post new data');
+    }
+};
+
+const updateUiWithRecentData = async () => {
+    const response = await fetch('/api/user-entry');
+    try {
+        const recentUserEntry = await response.json();
+        document.getElementById('date').innerHTML = recentUserEntry.date;
+        document.getElementById('temp').innerHTML = Math.round(recentUserEntry.temperature) + ' degrees';
+        document.getElementById('content').innerHTML = recentUserEntry.userResponse;
+    } catch (error) {
+        console.log('Can not retrieve current user response', error);
+    }
+};
+
+document.getElementById('generate').addEventListener('click', async () => {
+    let zipCode = document.getElementById('zip').value;
+
+    getTemperatureByZip(zipCode).then(
+        (temperature) => {
+            let recentUserEntry = buildRecentUserEntry(temperature);
+            postRecentUserEntry(recentUserEntry);
+            updateUiWithRecentData();
+        }
+    );
+});
